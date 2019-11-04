899706db4f6ea697418413667803e9d5e62d16c3
Phil Swartz <pswartz.sju@gmail.com>
2019-11-03 20:34:32 -0500

    updated apiroutes with some beginning logic to determine a best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,22 +8,53 @@ module.exports = function(app) {
     // from friends.js file
     app.get('/api/friendslist', (req, res) => {
 
-        // console.log(req.body);
-
         res.json(friendsData);
 
     });
 
 
-    // this post request saves the response and pushes it back
-    // to the client to be displayed in our API
+    // post request to save the response and pushes it back
+    // to the client to be displayed in our api/friendslist path
     app.post('/api/friendslist', function(req, res){
 
-        friendsData.push(req.body);
+        let bodyData = req.body; 
 
-        console.log(req.body);
+        // push latest user via req.body into the full friends list referenced above
+        friendsData.push(bodyData);
+
+        // variables to store the latest user's name, photo, and scores
+        let currentUserName = bodyData.name;
+        let currentUserPhoto = bodyData.photo;
+        let currentUserScore = bodyData.scores;
+
+        /* console.log(currentUserName);
+        console.log(currentUserPhoto);
+        console.log(currentUserScore); */
+
+        let bestFriendMatch = {
+            name: "",
+            photo: ""
+        }
+
+        let totalDifference = 0;
+
+        // loop through fulls friends list minus the current user
+        for (let i = 0; i < friendsData.length - 1; i++){
+
+            console.log(friendsData[i].name);
+
+
+        }
+
+
 
         res.json(friendsData);
+
+
+
+        // log the entire body array of objects to see what it looks like
+        // console.log(friendsData);
+        
         
         // res.json(true);
 
