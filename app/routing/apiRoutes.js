17557e443d20387024a597868d74da261b1917ff
Phil Swartz <pswartz.sju@gmail.com>
2019-11-04 21:58:10 -0500

    finished best friend match logic and appended it to the html modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,8 +20,8 @@ module.exports = function (app) {
         let bodyData = req.body;
 
         // variables to store the latest user's name, photo, and scores
-        let currentUserName = bodyData.name;
-        let currentUserPhoto = bodyData.photo;
+        // let currentUserName = bodyData.name;
+        // let currentUserPhoto = bodyData.photo;
         let currentUserScore = bodyData.scores;
 
         let bestFriendMatch = {
@@ -34,7 +34,7 @@ module.exports = function (app) {
         if (currentUserScore === undefined || currentUserScore.length == 0) {
 
             console.log('ending the program due to bad POST request' +
-                '\n' + 'user submitted survey without selecting scores to a question');
+                '\n' + 'user submitted survey without selecting answer to a question');
 
             res.end();
 
@@ -44,46 +44,55 @@ module.exports = function (app) {
             friendsData.push(bodyData);
 
 
-            // console.log(currentUserName);
-            // console.log(currentUserPhoto);
-            // console.log(currentUserScore.length);
-            // console.log(friendsData[0].scores);
-
-
-
-
             // loop through full friends list minus the current user
             for (let i = 0; i < friendsData.length - 1; i++) {
-
-                console.log(friendsData[i].name);
 
                 // loop through individual friends scores and compare againt current user
                 for (let j = 0; j < currentUserScore.length; j++) {
 
                     totalDifference += Math.abs(currentUserScore[j] - friendsData[i].scores[j])
 
-                    // console.log(totalDifference);
                 }
 
                 friendsData[i]["matchScore"] = totalDifference;
 
                 totalDifference = 0;
 
-                console.log(friendsData[i]);
+            }
+
+            // loop to find the friend with the lowest matchScore
+            let lowestScoreArr = [];
+            for (let i = 0; i < friendsData.length - 1; i++) {
+                
+                lowestScoreArr.push(parseInt(friendsData[i].matchScore));
 
             }
 
+            let lowestNum = Math.min(...lowestScoreArr);
+            let lowestNumIndex = lowestScoreArr.indexOf(lowestNum);
 
 
-            res.json(friendsData);
+            // console.log(lowestNum);
+            // console.log(lowestNumIndex);
+            // console.log(lowestScoreArr);
 
+            bestFriendMatch.name = friendsData[lowestNumIndex].name;
+            bestFriendMatch.photo = friendsData[lowestNumIndex].photo;
 
+            console.log(bestFriendMatch);
+
+            // res.json(friendsData);
+
+            // send the best friend Match back to the client via a json string
+            res.json(bestFriendMatch);
 
             // log the entire body array of objects to see what it looks like
             // console.log(friendsData);
 
-
         }
+
+
+
 
 
     });
